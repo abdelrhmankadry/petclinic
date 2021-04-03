c32c16ab12e4f5996575d2a2a473ce1353482de7
abdelrhmankadry <abdokadry00@gmail.com>
2021-04-04 01:09:00 +0200

    refactoring mapServices to generate ids

--- petclinic-data/src/main/java/com/kadry/petclinic/services/map/AbstractMapService.java
+++ petclinic-data/src/main/java/com/kadry/petclinic/services/map/AbstractMapService.java
@@ -1,21 +1,40 @@
 package com.kadry.petclinic.services.map;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import com.kadry.petclinic.model.BaseEntity;
 
-public abstract class AbstractMapService <T, ID>{
+import java.util.*;
 
-    protected Map<ID, T> map = new HashMap<>();
+public abstract class AbstractMapService <T extends BaseEntity, ID extends Long>{
+
+    protected Map<Long, T> map = new HashMap<>();
 
     Set<T> findAll(){ return new HashSet<>(map.values());}
 
     T findById(ID id){ return map.get(id);}
 
-    T save(ID id, T object){ return map.put(id, object);}
+    T save(T object){
+        if(object != null){
+            if(object.getId() == null){
+                object.setId(getNextId());
+            }
+        }else{
+            throw new RuntimeException ("object cannot be null");
+        }
+        return map.put(object.getId(), object);
+    }
 
     void deleteById(ID id) { map.remove(id);}
 
     void delete(T object){ map.entrySet().removeIf(entry -> entry.getValue().equals(object));}
+
+    private Long getNextId(){
+
+        Long nextId = null;
+        try{
+        nextId =  Collections.max(map.keySet()) +1;
+        }catch (NoSuchElementException e){
+            nextId = 1L;
+        }
+        return nextId;
+    }
 }
