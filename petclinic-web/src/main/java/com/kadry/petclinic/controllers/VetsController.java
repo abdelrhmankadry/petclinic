76e36d6599ca568532bcf27922aba2a58f26afac
abdelrhmankadry <abdokadry00@gmail.com>
2021-04-04 00:55:36 +0200

    list vets in owners/index page, update it's controller

--- petclinic-web/src/main/java/com/kadry/petclinic/controllers/VetsController.java
+++ petclinic-web/src/main/java/com/kadry/petclinic/controllers/VetsController.java
@@ -1,13 +1,23 @@
 package com.kadry.petclinic.controllers;
 
+import com.kadry.petclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class VetsController {
 
+    private final VetService vetService;
+
+    public VetsController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"/vets", "/vets/index.html", "/vets/index"})
-    public String index(){
+    public String index(Model model){
+
+        model.addAttribute("vets", vetService.findAll());
 
         return "vets/index";
     }
