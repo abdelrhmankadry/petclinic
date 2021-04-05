f65dcba96bb4464038e597131055ecc3986bff15
abdelrhmankadry <abdokadry00@gmail.com>
2021-04-05 03:39:40 +0200

    Update Owner, Person, Pet, and Pet Type with JPA annotations.

--- petclinic-data/src/main/java/com/kadry/petclinic/model/Owner.java
+++ petclinic-data/src/main/java/com/kadry/petclinic/model/Owner.java
@@ -1,13 +1,23 @@
 package com.kadry.petclinic.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name="owners")
 public class Owner extends Person{
 
+    @Column(name="address")
     private String address;
+
+    @Column(name="city")
     private String city;
+
+    @Column(name="telephone")
     private String telephone;
+
+    @OneToMany(cascade= CascadeType.ALL, mappedBy="owner")
     private Set<Pet> pets = new HashSet<>();
 
     public String getAddress() {
@@ -18,9 +28,7 @@ public class Owner extends Person{
         this.address = address;
     }
 
-    public String getCity() {
-        return city;
-    }
+    public String getCity() { return city; }
 
     public void setCity(String city) {
         this.city = city;
