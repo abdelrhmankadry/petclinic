8d20c635962c0faaaa12cefc0532e4ba15a89d3d
abdelrhmankadry <abdokadry00@gmail.com>
2021-04-28 16:24:05 +0200

    added json endpoint for vets

--- petclinic-web/src/main/java/com/kadry/petclinic/controllers/VetsController.java
+++ petclinic-web/src/main/java/com/kadry/petclinic/controllers/VetsController.java
@@ -1,9 +1,14 @@
 package com.kadry.petclinic.controllers;
 
+import com.kadry.petclinic.model.Vet;
 import com.kadry.petclinic.services.VetService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Set;
 
 @Controller
 public class VetsController {
@@ -20,5 +25,10 @@ public class VetsController {
         model.addAttribute("vets", vetService.findAll());
 
         return "vets/index";
+    }
+
+    @GetMapping("/api/vets")
+    public @ResponseBody Set<Vet> getVetsJson(){
+        return vetService.findAll();
     }
 }
